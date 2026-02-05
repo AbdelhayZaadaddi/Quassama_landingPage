5477fe6156bb74f72bee8fd54d02f83935d34975
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2026-02-05 09:37:15 +0100

    add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,12 @@ import Footer from "./components/Footer";
 import Reviews from "./components/Reviews";
 import FAQAndCTA from "./components/FAQAndCTA";
 import ComparisonTable from "./components/ComparisonTable";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
     <>
+      <Analytics />
       <Header />
       <Hero />
       <ProblemSolution />
